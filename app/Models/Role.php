0c56fff41395850e63749d6746dc7d2e2c875661
Sopanha Leavchum <leavchumsopanha@gmail.com>
2024-08-18 16:28:09 +0700

    add model and uuid column

---
+++ app/Models/Role.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $fillable = [
+        role_name
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
