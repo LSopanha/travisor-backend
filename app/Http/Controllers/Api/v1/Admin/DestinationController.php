08a479cfcf3f5598091eb39b1eff24f385f84c95
Sopanha Leavchum <leavchumsopanha@gmail.com>
2024-09-16 22:29:25 +0700

    manage destinations

---
+++ app/Http/Controllers/Api/v1/Admin/DestinationController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admin;
+
+use App\Http\Controllers\Api\v1\BaseAPI;
+use App\Http\Requests\Admin\StoreDestinationRequest;
+use App\Http\Requests\Admin\UpdateDestinationRequest;
+use App\Http\Resources\CountryResource;
+use App\Http\Resources\DestinationResource;
+use App\Http\Resources\UserResource;
+use App\Services\DestinationSV;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class DestinationController extends BaseAPI
+{
+    protected $destinationSV;
+    public function __construct()
+    {
+        $this->destinationSV = new DestinationSV();
+    }
+
+    public function index()
+    {
+        try {
+            $destinations = $this->destinationSV->getAllDestinations();
+            return $this->successResponse(DestinationResource::collection($destinations), 'Get all destiantions successfully.');
+        } catch(\Exception $e){
+            return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function store(StoreDestinationRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $params['destination_name'] = getParam($request->all(), 'destination_name');
+            $params['profile_picture']  = getParam($request->all(), 'profile_picture');
+            $params['description']      = getParam($request->all(), 'description');
+            $params['continent_id']     = getParam($request->all(), 'continent_id');
+            $params['country_id']       = getParam($request->all(), 'country_id');
+
+            $destination = $this->destinationSV->createNewDestination($params);
+            DB::commit();
+            return $this->successResponse($destination, 'Destiantion created successfully.');
+        } catch(\Exception $e){
+           return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function show($global_id)
+    {
+        try{
+            $destination = $this->destinationSV->getDestinationByGlobalId($global_id);
+            return $this->successResponse(new DestinationResource($destination), 'Get destination successfully.');
+
+        }catch(\Exception $e){
+            return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function update(UpdateDestinationRequest $request, $global_id)
+    {
+        try{
+            DB::beginTransaction();
+            $params['destination_name'] = getParam($request->all(), 'destination_name');
+            $params['profile_picture']  = getParam($request->all(), 'profile_picture');
+            $params['description']      = getParam($request->all(), 'description');
+            $params['continent_id']     = getParam($request->all(), 'continent_id');
+            $params['country_id']       = getParam($request->all(), 'country_id');
+
+            $destination = $this->destinationSV->UpdateDestination($params, $global_id);
+            DB::commit();
+            return $this->successResponse($destination, 'Destination updated successfully.');          
+        }catch(\Exception $e){
+            return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function deactivateDestination($global_id)
+    {
+        try{
+            $active = 0;
+            $destination = $this->destinationSV->deactivateDestination($global_id, $active);
+        }catch(\Exception $e){
+            return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+        return $this->successResponse($destination, 'Destination deactivated successfully.');
+    }
+    public function activateDestination($global_id)
+    {
+        try{
+            $active = 1;
+            $destination = $this->destinationSV->deactivateDestination($global_id, $active);
+        }catch(\Exception $e){
+            return $this->errorResponse($e->getMessage(), $e->getCode());
+        }
+        return $this->successResponse($destination, 'Destination activated successfully.');
+    }
+
+}
--- routes/admin.php
+++ routes/admin.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Api\v1\Admin\AdminController;
 use App\Http\Controllers\Api\v1\Admin\ClientController;
 use App\Http\Controllers\Api\v1\Admin\ContinentController;
 use App\Http\Controllers\Api\v1\Admin\CountryController;
+use App\Http\Controllers\Api\v1\Admin\DestinationController;
 use App\Http\Controllers\Api\v1\Admin\RoleController;
 use App\Http\Controllers\Api\v1\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
@@ -31,6 +32,10 @@ Route::group(['middleware' => 'auth:api', 'prefix' => 'auth/v1'], function ($rou
    Route::apiResource('/countries', CountryController::class);
    Route::put('countries/{id}/deactivate', [CountryController::class, 'deactivateCountry']);
    Route::put('countries/{id}/activate', [CountryController::class, 'activateCountry']);
+   Route::apiResource('/destinations', DestinationController::class);
+   Route::put('destinations/{id}/deactivate', [DestinationController::class, 'deactivateDestination']);
+   Route::put('destinations/{id}/activate', [DestinationController::class, 'activateDestination']);
+
 });
 
 Route::post('/login', [AuthController::class, 'loginAuthentication']);
